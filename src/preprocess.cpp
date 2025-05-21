1ce6f68679e331dbb37e35ce5cf12a8a038788ee
Jaime Sandoval <jaime.sandoval@vitom-tech.com>
2025-05-21 09:55:20 +0900

    Fix wrong point timestamp on xt32 pointclouds

--- src/preprocess.cpp
+++ src/preprocess.cpp
@@ -574,9 +574,11 @@ void Preprocess::xt32_handler(const sensor_msgs::msg::PointCloud2::UniquePtr &ms
   pl_surf.reserve(plsize);
 
   const double frame_timestamp = rclcpp::Time(msg->header.stamp).seconds();
+  const double blind2 = blind*blind;
 
   for(std::size_t i = 0; i < plsize; ++i)
-  {
+  { 
+
     PointType added_pt;
     added_pt.normal_x = 0;
     added_pt.normal_y = 0;
@@ -585,9 +587,9 @@ void Preprocess::xt32_handler(const sensor_msgs::msg::PointCloud2::UniquePtr &ms
     added_pt.y = pl_orig.points[i].y;
     added_pt.z = pl_orig.points[i].z;
     added_pt.intensity = pl_orig.points[i].intensity;
-    added_pt.curvature = (frame_timestamp - pl_orig.points[i].timestamp) * time_unit_scale;
+    added_pt.curvature = (pl_orig.points[i].timestamp - frame_timestamp) * time_unit_scale; 
 
-    if (added_pt.x * added_pt.x + added_pt.y * added_pt.y + added_pt.z * added_pt.z > (blind * blind))
+    if (added_pt.x * added_pt.x + added_pt.y * added_pt.y + added_pt.z * added_pt.z > blind2)
     {
       pl_surf.push_back(std::move(added_pt));
     }
